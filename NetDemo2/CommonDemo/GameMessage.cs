c452750fc6a724844856d4a252a74da3c2a94816
chenshenchao <chenshenchao@outlook.com>
2022-09-08 11:08:35 +0800

    save

---
+++ NetDemo2/CommonDemo/GameMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CommonDemo;
+
+public class GameMessage
+{
+    public GameMessageKind Kind { get; set; }
+    public long PlayerId { get; set; }
+
+    public string ToJsonString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
---
+++ NetDemo2/Readme.md
@@ -0,0 +1,5 @@
+# Net Demo 2
+
+nuget.org
+Grpc
+Grpc.Tools

954033e397f764ae6adfb5e00ee7be8d61d8391e
chenshenchao <chenshenchao@outlook.com>
2022-10-28 20:46:22 +0800

    save

--- NetDemo2/ClientDemo/Program.cs
+++ NetDemo2/ClientDemo/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Client Demo Start!");
+﻿using ClientDemo.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = Host.CreateDefaultBuilder(args)
+    .UseConsoleLifetime()
+    .ConfigureHostConfiguration(cd =>
+    {
+        cd.SetBasePath(Directory.GetCurrentDirectory());
+        cd.AddEnvironmentVariables(prefix: "YZ_ROBOT_");
+    })
+    .ConfigureLogging((hc, cl) =>
+    {
+        cl.AddFile(hc.Configuration.GetSection("LoggingFile"));
+        cl.AddConsole();
+    })
+    .ConfigureServices((hc, services) =>
+    {
+        services.AddHostedService<AutoClientsService>();
+    })
+    .Build();
+
+host.Run();
---
+++ NetDemo2/ClientDemo/Services/AutoClientsService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientDemo.Services;
+
+public class AutoClientsService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Yield();
+        }
+    }
+}
